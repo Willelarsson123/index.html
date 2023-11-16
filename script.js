f11d5e727b688e27e3acfcc0075580ec32eb4071
Willelarsson123 <wille_larsson123@live.com>
2023-11-16 14:13:09 +0100

    d

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
         document.getElementById("searchBtn").addEventListener("click", function() {
             var searchInputValue = document.getElementById("searchInput").value.toLowerCase();
 
-            // Sök på startsidan
+  
             var offerSection = document.querySelector(".offer-section");
             if (offerSection.textContent.toLowerCase().includes(searchInputValue)) {
                 offerSection.style.display = "block";
@@ -10,7 +10,7 @@
                 offerSection.style.display = "none";
             }
 
-            // Sök på produktsidan
+  
             var productTableRows = document.querySelectorAll(".product-row");
             productTableRows.forEach(function(row) {
                 if (row.textContent.toLowerCase().includes(searchInputValue)) {
@@ -20,7 +20,7 @@
                 }
             });
 
-            // Sök på mediasektionen
+  
             var mediaSection = document.querySelector(".media-section");
             if (mediaSection.textContent.toLowerCase().includes(searchInputValue)) {
                 mediaSection.style.display = "block";
@@ -29,18 +29,17 @@
             }
         });
     </script>
-// script.js
+
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Hämta referenser till sökrutan och tabellen
+
     var searchInput = document.querySelector('.search-container input');
     var rows = document.querySelectorAll('table tr');
 
-    // Lyssna på ändringar i sökrutan
+
     searchInput.addEventListener('input', function () {
         var searchTerm = searchInput.value.toLowerCase();
 
-        // Loopa igenom varje rad och göm eller visa beroende på söktermen
         rows.forEach(function (row) {
             var textContent = row.textContent.toLowerCase();
             if (textContent.includes(searchTerm)) {
